9e97bd5a2b4926fe3aa7c4f1dd8a01990542f209
namheheh <105479816+namheheh@users.noreply.github.com>
2023-05-28 10:14:26 +0700

    update_component

--- asm1_we17302/src/app/app-routing.module.ts
+++ asm1_we17302/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductListComponent } from './pages/admin/product-list/product-list.component';
+import { ProductAddComponent } from './pages/admin/product-add/product-add.component';
+import { ProductEditComponent } from './pages/admin/product-edit/product-edit.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent },
+      { path: "product", component: ProductListComponent },
+      { path: "product/add", component: ProductAddComponent },
+      { path: "product/:id/edit", component: ProductEditComponent },
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- asm1_we17302/src/app/app.module.ts
+++ asm1_we17302/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductAddComponent } from './pages/admin/product-add/product-add.component';
+import { ProductListComponent } from './pages/admin/product-list/product-list.component';
+import { ProductEditComponent } from './pages/admin/product-edit/product-edit.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminProductComponent,
+    DashboardComponent,
+    ProductAddComponent,
+    ProductListComponent,
+    ProductEditComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
